e126fe8e925d548b77bdaa049f1766a300c44942
Liza George <liza@chiramattel.com>
2021-02-23 22:57:37 -0800

    CSES Problem Set Task 1084: Apartments - NOT COMPLETE
    I see the issue with my solution, currently it doesn't take into account what happens if there are multiple apartments with the same size. I briefly looked at the solution, looks like it needs a knowledge of greedy algorithms. I'll study that and then come back to this.

--- repeater/repeater.cpp
+++ repeater/repeater.cpp
@@ -3,8 +3,8 @@
 // Practicing Using Different Data Structures
 // February 23rd, 2021
 
-// CSES Problem Set Task 1621: Distinct Numbers
-// https://cses.fi/problemset/task/1621
+// CSES Problem Set Task 1084: Apartments
+// https://cses.fi/problemset/task/1084
 
 
 
@@ -29,24 +29,41 @@ int main()
 
     //---------------------BEGIN---------------------//
 
+    int ans = 0;
+
     int n; cin >> n;
+    int m; cin >> m;
+    int k; cin >> k;
 
-    set<int> s;
-
+    vector<int> des;
     for (int i = 0; i < n; i++)
     {
-        int insert; cin >> insert;
-        s.insert(insert);
+        int x; cin>>x;
+        des.push_back(x);
     }
 
-    int ans = 0;
-    for(int i:s){
-        ans++;
+    set<int> act;
+    for(int i = 0; i < m; i++)
+    {
+        int x; cin>>x;
+        act.insert(x);
+    }
+
+    for(int d:des){
+        for(int t = d-k; t < d+k+1; t++){
+            if(act.count(t) == 1){
+                act.erase(t);
+                ans++;
+                // cout<<"accepted des = " << d << endl;
+                break;
+            }
+        }
     }
     
+    
+
     cout << ans;
-    
-    
+        
     return 0;
 
 }
--- repeater/standard-i-o.cpp
+++ repeater/standard-i-o.cpp
@@ -9,23 +9,40 @@ int main()
 {
     //---------------------BEGIN---------------------//
 
+     int ans = 0;
+
     int n; cin >> n;
+    int m; cin >> m;
+    int k; cin >> k;
 
-    set<int> s;
-
+    vector<int> des;
     for (int i = 0; i < n; i++)
     {
-        int insert; cin >> insert;
-        s.insert(insert);
+        int x; cin>>x;
+        des.push_back(x);
     }
 
-    int ans = 0;
-    for(int i:s){
-        ans++;
+    set<int> act;
+    for(int i = 0; i < m; i++)
+    {
+        int x; cin>>x;
+        act.insert(x);
+    }
+
+    for(int d:des){
+        for(int t = d-k; t < d+k+1; t++){
+            if(act.count(t) == 1){
+                act.erase(t);
+                ans++;
+                // cout<<"accepted des = " << d << endl;
+                break;
+            }
+        }
     }
     
-    cout << ans;
     
+
+    cout << ans;
     
     return 0;
 
